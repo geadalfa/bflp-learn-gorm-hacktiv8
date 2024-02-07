65885fc297490042b62cceda68d41ac9595e3bf3
Geadalfa Giyanda <ggeadalfa@gmail.com>
2024-02-07 11:49:10 +0700

    crud in gorm

---
+++ models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        uint   `gorm:"primary key"`
+	Email     string `gorm:"not null; unique; type: varchar(191)"`
+	Products  []Product
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
